d09bf73539ff2d24924484e7ceb61bd121780f8c
Craig Robertson <craig_c_robertson@yahoo.co.uk>
2025-06-28 15:55:22 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mob-we-recruit',
+  webDir: 'www'
+};
+
+export default config;
